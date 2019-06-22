09ffbaf1557e08c79f82055970f7fb5cf556d6ef
Maxim Galaganov <m.galaganov@corp.mail.ru>
2019-06-23 02:14:55 +0300

    shine createrepo automatic

--- site-modules/shine/manifests/init.pp
+++ site-modules/shine/manifests/init.pp
@@ -1,7 +1,19 @@
 class shine(
   Boolean $enable = true,
+  String $createrepo_version = 'present',
 ) {
 
+  package { 'createrepo' :
+    ensure => $createrepo_version,
+  }
+
+  file { '/opt/shine' :
+    ensure => 'directory',
+  }->
+  exec { '/usr/bin/createrepo --update /opt/shine' :
+    refreshonly => true,
+    require => Package['createrepo'],
+  }->
   yumrepo { 'shine-local' :
     enabled => $enable,
     name => 'shine',
